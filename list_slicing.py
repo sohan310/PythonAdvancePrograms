2e36d9dbfa49f9d1a87a3a845661d7dfaf7c19c2
Sohan-kun <79075374+sohan0310grd@users.noreply.github.com>
2021-02-21 16:05:23 +0530

    Add files via upload

---
+++ list_slicing.py
@@ -0,0 +1,11 @@
+a=[1,2,3,4,5,6,7,8,9]
+print(a)
+#-----START_&_STEP_&_STOP----
+
+print(a[0:5],"\n")
+print(a[1:4],"\n")
+print("JUMPING BY 2 NUMBERS : ",a[0:5:2],"\n")
+print("JUMPING BY 2 NUMBERS : ",a[1:7:2],"\n")
+print("ODD",a[::2],"\n")
+print("EVEN",a[1::2],"\n")
+print("-VE Index",a[::-2],"\n")
